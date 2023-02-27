28963d4cf3ba73c33dfdee70dcaa7a790f5ae63b
Fakhri Permana <kripermana1107@gmail.com>
2023-02-27 14:51:09 +0700

    Changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
+import NavbarComp from './components/NavbarComp';
+import HeroComp from './components/HeroComp';
+import GalleryComp from './components/GalleryComp';
+import ServicesComp from './components/ServicesComp';
+import FaqComp from './components/FaqComp';
+import FooterComp from './components/FooterComp';
+
 
 function App() {
-  return <div></div>;
+  return (<div>
+
+    <HeroComp />
+
+    {/* Content */}
+
+     <NavbarComp />
+
+    <GalleryComp />
+    {/* <ServicesComp />
+    <FaqComp /> */}
+
+    
+    {/* <FooterComp /> */}
+
+
+  </div>
+  )
+;
 }
 
 export default App;
