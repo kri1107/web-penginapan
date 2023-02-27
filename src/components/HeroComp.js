e23938b5a65fa45f61b0b2d9da76437b8edbaf3d
Fakhri Permana <kripermana1107@gmail.com>
2023-02-27 14:50:57 +0700

    Added Hero

---
+++ src/components/HeroComp.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Container, Row, Col} from "react-bootstrap";
+
+const HeroComp = () => {
+    return (
+         <div className="hero min-vh-100 w-100">
+        <Container>
+            <Row>
+                <Col className="text-white text-center">
+                    <h1>Mari Bersantai Sejenak</h1>
+                    <p className="text-white-50">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus varius neque nunc, at convallis dui commodo ac. Nulla tristique, lectus et dapibus tempus, massa urna aliquet diam, non dapibus metus lacus in mauris.</p>
+                </Col>
+            </Row>
+        </Container>
+    </div>
+    );
+};
+
+export default HeroComp;
